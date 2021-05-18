794c9c05f9b46c6a5d6c41c98c7dcfc51354c7ec
Mariano Varela <marianogvarela@gmail.com>
2021-05-18 19:15:44 -0300

    catch exceptions

--- src/main/java/ar/com/qbuilder/service/SparkService.java
+++ src/main/java/ar/com/qbuilder/service/SparkService.java
@@ -41,17 +41,20 @@ public class SparkService {
 	}
 
 	public void writeObject(Datasource datasource, String table, List<ar.com.qbuilder.domain.Object> objects) {
-		SparkSession spark = SparkSession.builder().appName("Sp_LogistcRegression").master("local").getOrCreate();
-		SQLContext sqlContext = spark.sqlContext();
-		JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
-
-		JavaRDD<ar.com.qbuilder.domain.Object> objRDD = jsc.parallelize(objects);
-		Dataset<Row> objDf = sqlContext.createDataFrame(objRDD, ar.com.qbuilder.domain.Object.class);
-
-		Properties properties = new java.util.Properties();
-		objDf.write().mode(SaveMode.Append).jdbc(datasource.getUrl() + "/" + datasource.getSchema() + "?user="
-				+ datasource.getUser() + "&password=" + datasource.getPassword(), table, properties);
-
+		try {
+			SparkSession spark = SparkSession.builder().appName("Sp_LogistcRegression").master("local").getOrCreate();
+			SQLContext sqlContext = spark.sqlContext();
+			JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+			
+			JavaRDD<ar.com.qbuilder.domain.Object> objRDD = jsc.parallelize(objects);
+			Dataset<Row> objDf = sqlContext.createDataFrame(objRDD, ar.com.qbuilder.domain.Object.class);
+			
+			Properties properties = new java.util.Properties();
+			objDf.write().mode(SaveMode.Append).jdbc(datasource.getUrl() + "/" + datasource.getSchema() + "?user="
+					+ datasource.getUser() + "&password=" + datasource.getPassword(), table, properties);
+		} catch(Exception  e) {
+			e.printStackTrace();
+		}
 	}
 
 	public Object updateObject(Datasource datasource, String table, List<ar.com.qbuilder.domain.Object> objects) {
